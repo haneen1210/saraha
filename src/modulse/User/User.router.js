e8ff4d7e9d4ac5749a74d3f75a5cc3c63beed948
haneen <haneenemail@gmail.com>
2024-02-09 19:47:55 +0200

    upload vercel file

--- src/modulse/User/User.router.js
+++ src/modulse/User/User.router.js
@@ -1,5 +1,5 @@
 import  express  from "express";
-import * as UserControler from './Controller/User.controller.js';
+import * as UserControler from './controller/User.controller.js';
 import {auth} from '../../middleware/Auth.middleware.js';
 import {asynHandler} from '../../middleware/errorHanding.js';
 const app=express();
