df8d72a0af0eaede64c1b95b1c76ceff3f5506e3
haneen <haneenemail@gmail.com>
2024-02-10 23:12:07 +0200

    upload vercel file

--- src/middleware/errorHanding.js
+++ src/middleware/errorHanding.js
@@ -2,7 +2,7 @@ export const asynHandler = (fun) => {
     return (req, res, next) => {
         fun(req, res, next).catch((error) => {
             //return res.status(500).json({ message: "catch error", error: error.stack });
-            return next(new Error(error.stack));
+            return next(new Error(error));
         })
     }
 }
