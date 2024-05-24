36f343daed79d52d9329ecaba7069380d355df2d
haneen <haneenemail@gmail.com>
2024-05-24 23:29:45 +0300

    uplod file

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ import initApp from "./src/modulse/app.router.js";
 const app=express();
 const PORT=process.env.PORT|| 3000;
 initApp(app,express);
+app.get('/',(req,res)=>{
+    return res.json("welcome");
+})
 app.listen(PORT,()=>{
     console.log(`server is running at ${PORT}`);
 })
