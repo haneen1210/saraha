08ce984829334ddfbf7793cd98c38b650f0d3c58
haneen <haneenemail@gmail.com>
2024-05-25 00:04:25 +0300

    uplod file

--- src/modulse/User/user.validation.js
+++ src/modulse/User/user.validation.js
@@ -8,7 +8,7 @@ export const updatPassword ={
     body:Joi.object({
         oldPassword:generalFields.password,
         newPassword :generalFields.password.invalid(Joi.ref('oldPassword')),
-        Cpassword:Joi.string().valid(Joi.ref('password')).required(),
+        Cpassword:Joi.string().valid(Joi.ref('newPassword')).required(),
     }),
 }
 export const shareProfile ={
